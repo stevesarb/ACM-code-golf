637de67bd77a6ce4943c96b9d8f5f1dbd7bb6211
Seba <sebastiantorresola3@gmail.com>
2025-04-10 19:19:10 -0700

    OBLIVIONNEEDDD

--- dateshyt.py
+++ dateshyt.py
@@ -1,1 +1,1 @@
-y,m,d=map(int,input().split("-"));if m in (1,2):y-=1;m+=12;K,J=y%100,y//100;l=['Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday'];print(l[(d+13*(m+1)//5+K+K//4+J//4+5*J)%7])
+y,m,d=map(int,input().split('-'));m+=m<3 and 12;y-=m>12;print('Saturday Sunday Monday Tuesday Wednesday Thursday Friday'.split()[(d+13*(m+1)//5+y+y//4-y//100+y//400)%7])
